c8b7ee18b0152f814c954264a12e6f568df5c95f
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2024-02-16 16:04:02 +0200

    responsive navbar

--- src/components/Navabr.jsx
+++ src/components/Navabr.jsx
@@ -3,6 +3,8 @@ import bostaLogo from "../assets/BostaLogo.png";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoSearchOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import { useState } from "react";
 
 import { useDispatch } from "react-redux";
@@ -12,6 +14,8 @@ const Navabr = () => {
   const [isHovering, setIsHovering] = useState(false);
   const [isSearchCardVisible, setIsSearchCardVisible] = useState(false);
   const [trackingNumber, setTrackingNumber] = useState("");
+  const [isHamburgerNavbarVisible, setIsHamburgerNavbarVisible] =
+    useState(false);
   const dispatch = useDispatch();
 
   const handleMouseEnter = () => {
@@ -24,20 +28,29 @@ const Navabr = () => {
 
   const toggleSearchCard = () => {
     setIsSearchCardVisible(!isSearchCardVisible);
+    setIsHamburgerNavbarVisible(false);
   };
 
   const handleSearchButtonClick = () => {
     setIsSearchCardVisible(false);
     dispatch(fetchPackageDetails(trackingNumber));
   };
+
+  const toggleHamburgerNavbar = () => {
+    setIsHamburgerNavbarVisible(!isHamburgerNavbarVisible);
+    setIsSearchCardVisible(false);
+  };
   return (
     <>
-      <nav>
+      <nav className="hidden md:flex">
         <div className="left-nav-section">
           <a href="#" className="nav-lang-selection">
             ENG
           </a>
           <a href="#">تسجيل الدخول</a>
+          <a className="cursor-pointer flex md:hidden">
+            <FaBars className="text-[1.5rem]" />
+          </a>
           <a
             className="nav-tracking-package"
             onMouseEnter={handleMouseEnter}
@@ -48,13 +61,37 @@ const Navabr = () => {
             <a href="#">تتبع شحنتك</a>
           </a>
         </div>
-        <div className="middle-nav-section">
+        <div className="middle-nav-section ">
           <a href="#">كلم المبيعات</a>
           <a href="#">الأسعار</a>
           <a href="#">الرئيسية</a>
         </div>
         <div className="nav-container">
-          <img src={bostaLogo} alt="logo" className="nav-logo" />
+          <img src={bostaLogo} alt="logo" className="nav-logo w-[6rem]" />
+        </div>
+      </nav>
+      {/* small screens navbar */}
+      <nav className="flex md:hidden py-[1.7rem] gap-[5.5rem]">
+        <div className="left-nav-section">
+          <a className="cursor-pointer " onClick={toggleHamburgerNavbar}>
+            {isHamburgerNavbarVisible ? (
+              <IoClose className="text-[1.5rem]" />
+            ) : (
+              <FaBars className="text-[1.5rem]" />
+            )}
+          </a>
+          <a
+            className="nav-tracking-package"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={toggleSearchCard}
+          >
+            {isHovering ? <IoIosArrowDown /> : <IoIosArrowBack />}
+            <a href="#">تتبع شحنتك</a>
+          </a>
+        </div>
+        <div className="nav-container">
+          <img src={bostaLogo} alt="logo" className="nav-logo w-[6rem]" />
         </div>
       </nav>
       {isSearchCardVisible && (
@@ -77,6 +114,19 @@ const Navabr = () => {
                 }}
               />
             </div>
+          </div>
+        </div>
+      )}
+      {isHamburgerNavbarVisible && (
+        <div className="hamburger-navbar flex md:hidden">
+          <div className="middle-nav-section">
+            <a href="#">الرئيسية</a>
+            <a href="#">الأسعار</a>
+            <a href="#">كلم المبيعات</a>
+            <a href="#">تسجيل الدخول</a>
+            <a href="#" className="nav-lang-selection">
+              ENG
+            </a>
           </div>
         </div>
       )}
