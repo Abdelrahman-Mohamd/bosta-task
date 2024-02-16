9b4d12623156d459de692242c48ed42b39a950b3
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2024-02-16 09:40:58 +0200

    finished the task, not responsive, not translated

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -2,6 +2,7 @@ import "./DetailsCard.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchPackageDetails } from "../rtk/slices/package-slice";
+import questionLogo from "../assets/Questions-amico.png";
 
 const DetailsCard = () => {
   const state = useSelector((state) => state.package);
@@ -40,37 +41,64 @@ const DetailsCard = () => {
 
   return (
     <>
-      <div className="table-container">
-        <h1>تفاصيل الشحنة</h1>
-        <div className="table">
-          <div className="row">
-            <div className="cell">التفاصيل</div>
-            <div className="cell">الوقت</div>
-            <div className="cell">التاريخ</div>
-            <div className="cell">الفرع</div>
-          </div>
-          {state.data.TransitEvents.map((event, index) => (
-            <div className="row" key={index + 2}>
-              <div className="cell">
-                {formatState(event.state)}{" "}
-                {event.state === "CANCELLED" && (
-                  <h5 className="text-[#ff0000] text-[0.7vw]">
-                    تم الغاء الشحنة من التاجر
-                  </h5>
-                )}
-                {event.state === "DELIVERED_TO_SENDER" && (
-                  <h5 className="text-yellow-400 text-[0.7vw]">
-                    العميل غير متواجد في العنوان
-                  </h5>
-                )}
+      <div className="details-card">
+        <div className="table-container">
+          <h1>تفاصيل الشحنة</h1>
+          <div className="table">
+            <div className="row">
+              <div className="cell text-[0.9rem] font-[700] text-[#8f8f8f]">
+                تفاصيل
               </div>
-              <div className="cell">{formatTime(event.timestamp)}</div>
-              <div className="cell">
-                {event.timestamp && event.timestamp.slice(0, 10)}
+              <div className="cell text-[0.9rem] font-[700] text-[#8f8f8f]">
+                الوقت
               </div>
-              <div className="cell">{event.hub}</div>
+              <div className="cell text-[0.9rem] font-[700] text-[#8f8f8f]">
+                التاريخ
+              </div>
+              <div className="cell text-[0.9rem] font-[700] text-[#8f8f8f]">
+                الفرع
+              </div>
             </div>
-          ))}
+            {state.data.TransitEvents.map((event, index) => (
+              <div className="row" key={index + 2}>
+                <div className="cell">
+                  {formatState(event.state)}{" "}
+                  {event.state === "CANCELLED" && (
+                    <h5 className="text-[#ff0000] text-[0.7vw]">
+                      تم الغاء الشحنة من التاجر
+                    </h5>
+                  )}
+                  {event.state === "DELIVERED_TO_SENDER" && (
+                    <h5 className="text-yellow-400 text-[0.7vw]">
+                      العميل غير متواجد في العنوان
+                    </h5>
+                  )}
+                </div>
+                <div className="cell">{formatTime(event.timestamp)}</div>
+                <div className="cell">
+                  {event.timestamp && event.timestamp.slice(0, 10)}
+                </div>
+                <div className="cell">{event.hub}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="extra-details">
+          <h1>عنوان التسليم</h1>
+          <div className="address">
+            <p>
+              امبابة شارع طلعت حرب مدينة العمال بجوار البرنس منزل 17 بلوك 33
+            </p>
+          </div>
+          <div className="complain-card">
+            <div className="left-section">
+              <h1>هل يوجد مشكلة في شحنتك ؟</h1>
+              <button>ابلاغ عن مشكلة</button>
+            </div>
+            <div className="right-section">
+              <img src={questionLogo} alt="question mark" className="w-[5vw]" />
+            </div>
+          </div>
         </div>
       </div>
     </>
